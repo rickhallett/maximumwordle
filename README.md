ec6400484af57894c4bd22316682e8012a1d179d
Rick Hallett <rick.hallett@brandwatch.com>
2022-12-25 12:22:33 +0000

    feat: add Word.ts for individual word helpers

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 ## Optimisations
 1) Are there more efficient ways of finding multiple items within a list?
 2) What is faster, `for` or `forEach`?
-3) 
+3) Is there a trade off between operating on a list of strings, or a list of objects representing those strings, that itself has helper methods on?
 
 ## Ideas
 1) Handle all wordlist operations on array copies to prevent state mutation bugs
---
+++ src/Word.ts
@@ -0,0 +1,68 @@
+export type Rarity = {
+  char: string;
+  val: number;
+};
+
+export type RarityList = Rarity[];
+
+export enum Indicator {
+  "GREY",
+  "YELLOW",
+  "GREEN",
+}
+
+export type Clue = {
+  char: string;
+  color: Indicator;
+  index: number;
+};
+
+export type ClueList = Clue[];
+
+export class Word {
+  #value: string = "";
+
+  constructor(word: string) {
+    this.#value = word;
+  }
+
+  get value() {
+    return this.#value;
+  }
+
+  hasGreenLetters(clueList: ClueList): boolean {
+    return false;
+  }
+
+  getGreenLetterCount(clueList: ClueList): number {
+    return 0;
+  }
+
+  hasYellowLetters(clueList: ClueList): boolean {
+    return false;
+  }
+
+  getYellowLetterCount(clueList: ClueList): number {
+    return 0;
+  }
+
+  hasGreyLetters(clueList: ClueList): boolean {
+    return false;
+  }
+
+  getGreyLetterCount(clueList: ClueList): number {
+    return 0;
+  }
+
+  getLetterRarityScore(wordList: string[]): number {
+    // return numeric value for sorting lists
+    // Rarity passed in as this will be dynamic depending on list
+    const rarity: RarityList = this.computeNewRarityValues(wordList);
+    return 0;
+  }
+
+  computeNewRarityValues(wordList: string[]): RarityList {
+    // const r = wordList.map(word => )
+    return [];
+  }
+}
